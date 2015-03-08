6146a714eec17fb49d09eafc241e1ce0d8fe8647
n00bmat <accounts@n00bworks.com>
2015-03-07 19:18:39 -0500

    config.sh cleanup

--- Server/config.sh
+++ Server/config.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 apt-get update &> /dev/null
-apt-get upgrade &> /dev/null
-apt-get install php5-fpm -y &> /dev/null
-apt-get install nginx -y &> /dev/null
+apt-get install php5-fpm nginx -y &> /dev/null
+cp -ra /vagrant/Web /var/www/ &> /dev/null
 cp /vagrant/Server/nginx /etc/nginx/sites-available/nginx &> /dev/null
 ln -s /etc/nginx/sites-available/nginx /etc/nginx/sites-enabled/
 rm -rf /etc/nginx/sites-enabled/default
 rm -rf /etc/nginx/sites-available/default
-cp -ra /vagrant/Web /var/www/ &> /dev/null
 service nginx restart &> /dev/null
 
 echo "Provisioning Done"
